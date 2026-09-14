ca1d6e933b4a516e02d6a1984d182dd0e45e39fc
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2026-09-14 21:57:12 +0530

    fix padding for non-verified listings

--- frontend/src/routes/listings/[listing_id]/+page.svelte
+++ frontend/src/routes/listings/[listing_id]/+page.svelte
@@ -104,7 +104,7 @@
 
 						{#if listing.is_live}
 							<span
-								class="inline-flex items-center rounded-full bg-[#edf5ed] px-2.5 py-1 text-xs font-extrabold text-[#327144]"
+								class="inline-flex items-center rounded-full bg-[#edf5ed] py-1 text-xs font-extrabold text-[#327144]"
 								>● Live</span
 							>
 						{/if}
